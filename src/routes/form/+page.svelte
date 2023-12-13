2691bb92ee21e92234bd5be0257f75d22c43fa32
alokup60 <alokrajsngh@gmail.com>
2023-12-13 19:02:30 +0530

    import actions form server page i.e is not possible

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,10 +1,20 @@
 <script>
+    // import {actions} from "./+page.server"
 export let form;
-console.log(form)
+console.log(form);
+
+// async function handleSubmit(){
+// 	const res = await actions.default({cookies, request});
+	
+// 	if(res.err){
+// 		console.error("Error: ",res.err);
+// 	}
+// }
+
 </script>
-{form?.name}
+<!-- {form?.name}
 {form?.email}
-{form?.password}
+{form?.password} -->
 
 <svelte:head>
 	<title>Form</title>
@@ -12,20 +22,23 @@ console.log(form)
 </svelte:head>
 
 <section class="w-8/12 flex mx-auto">
-	<form class="flex flex-col gap-3 justify-between mx-auto items-center" method="post">
-        <p></p>
+	<form class="flex flex-col gap-3 justify-between mx-auto items-center" method="post" on:submit={handleSubmit}>
+        
 		<div class="flex justify-between gap-2">
 			<label class="text-center">Name</label>
-			<input type="text" name="name"  class="border rounded-md"/>
+			<input type="text" name="name" value={form?.name ?? ''} class="border rounded-md"/>
+            <!-- <p>{err}</p> -->
 		</div>
 		<div class="flex justify-between gap-2">
 			<label class="text-center">Email</label>
-			<input type="text" name="email"  class="border rounded-md "/>
+			<input type="text" name="email" value={form?.email ?? ''} class="border rounded-md "/>
 		</div>
 		<div class="flex justify-between gap-2">
 			<label class="text-center">Password</label>
-			<input type="password" name="password" class="border rounded-md "/>
+			<input type="password" name="password" value={form?.password ?? ''} class="border rounded-md "/>
+            <!-- <p>{err}</p> -->
 		</div>
 		<button class="bg-green-400 px-3 py-1 rounded-md text-white">Submit</button>
 	</form>
 </section>
+
